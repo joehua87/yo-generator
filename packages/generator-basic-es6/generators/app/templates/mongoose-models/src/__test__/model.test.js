694474bf35035449638f352f88fb4cdbf01edd8b
joehua87 <joehua87@gmail.com>
2016-06-20 18:30:25 +0700

    [mongoose-models] check connect to mongo instance

--- packages/generator-basic-es6/generators/app/templates/mongoose-models/src/__test__/model.test.js
+++ packages/generator-basic-es6/generators/app/templates/mongoose-models/src/__test__/model.test.js
@@ -1,13 +1,22 @@
 import mongoose from 'mongoose'
 import { expect } from 'chai'
-import * as models from '../index'
+import * as createModelFunc from '../index'
+
+const dbHost = process.env.DB_HOST || 'localhost/test'
 
 describe('Models', () => {
-  const createFns = Object.keys(models).filter(name => name.match(/^create/))
+  let connection
+  it('connect to MongoDb', () => {
+    connection = mongoose.createConnection(dbHost)
+  })
+
+  const createFns = Object.keys(createModelFunc).filter(name => name.match(/^create/))
   for (const createFn of createFns) {
-    it(createFn, () => {
-      const Model = models[createFn](mongoose)
+    it(createFn, async() => { // eslint-disable-line
+      const Model = createModelFunc[createFn](connection)
       expect(Model.modelName).to.equal(createFn.replace(/^create/, '').replace(/Model$/, ''))
+      const items = await Model.find({}).lean()
+      expect(items).to.deep.equal([])
     })
   }
 })
